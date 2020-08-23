051405fad33ca5e42d6c5ae29a4a99e176dfd6fc
BrunoJesus84 <bruno.jesus@ic.ufal.br>
2020-08-23 14:43:42 +0100

    Updating scripts

--- pub-mqtt-client-complete.py
+++ pub-mqtt-client-complete.py
@@ -8,8 +8,16 @@ def on_publish(client,userdata,result):             #create function for callbac
 client1= paho.Client("control1")            #create client object
 client1.on_publish = on_publish             #assign function to callback
 client1.connect(broker,port)                #establish connection
-message = input("Enter the message to publish: ")
-topic = input("Enter the topic to publish: ")
-qos = int(input("Enter the QoS level: "))
-ret= client1.publish(topic,message,qos=qos,retain=False)    #publish
-print("message <", message, "> was published on topic <", topic, ">")
+cont = True
+while(cont):
+	message = input("Enter the message to publish: ")
+	topic = input("Enter the topic to publish: ")
+	qos = int(input("Enter the QoS level: "))
+	ret= client1.publish(topic,message,qos=qos,retain=False)    #publish
+	print("...")
+	print("message <", message, "> was published on topic <", topic, ">")
+	print("...")
+	opc = input("Continue to Publishing (Y/n): ")
+	if ((opc=="n") or (opc=="N")):
+		cont = False
+		print("Connection finished.")
--- sub-mqtt-client.py
+++ sub-mqtt-client.py
@@ -2,29 +2,29 @@ import paho.mqtt.client as mqtt #import the client1
 import time
 ############
 def on_message(client, userdata, message):
-    print("message received " ,str(message.payload.decode("utf-8")))
-    print("message topic=",message.topic)
-    print("message qos=",message.qos)
-    print("message retain flag=",message.retain)
+    print("\nmessage received: ",str(message.payload.decode("utf-8")))
+    print("message topic =",message.topic)
+    print("message QoS =",message.qos)
+    print("message retain flag =",message.retain)
 ########################################
 #broker_address="192.168.1.73" 
 cont = True
 while(cont):	
 	broker_address="broker.hivemq.com"
-	print("creating new instance")
+	print("creating new instance...")
 	client = mqtt.Client("B1") #create new instance
 	client.on_message = on_message #attach function to callback
-	print("connecting to broker")
+	print("connecting to broker...")
 	client.connect(broker_address) #connect to broker
 	print("...")
 	topic = input("Enter the topic to subscribe: " )
+	t = int(input("Enter the time (in seconds) of the connection: " ))
+	qos = int(input("Enter the QoS level: "))
 	client.loop_start() #start the loop
 	print("Subscribing to topic:",topic)
-	client.subscribe(topic)
+	client.subscribe(topic, qos=qos)
 	print("...")
-	#print("Publishing message to topic",topic)
-	#client.publish(topic,"Temperature X")
-	time.sleep(10) # wait
+	time.sleep(t) # wait
 	client.loop_stop() #stop the loop
 	opc = input("Continue to Subscribing (Y/n): ")
 	if ((opc=="n") or (opc=="N")):
